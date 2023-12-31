d331ac8b1cccd88b40a07995e85551be79e0c9e3
Iupac120 <oforokpara@gmail.com>
2023-12-31 03:13:09 +0100

    added nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 
 import './App.css';
-import Home from './Components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Signup from './Components/Signup';
 
 function App() {
   return (
-   <Router>
+   
        <div className="App">
-          <Navbar/>
-          <Switch>
-            <Route path='/' exact>
-              <Home/>
-            </Route>
-            <Route path='/signup' exact>
-              <Signup/>
-            </Route>
-          </Switch>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Navbar/>}/>
+            <Route path='/signup' element={<Signup/>}/>
+          </Routes>
+          </Router>
         </div>
-   </Router>
+   
   );
 }
 
