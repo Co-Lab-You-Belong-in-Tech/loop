04ca25c506fea00355a646c2d03af38eb2785eb4
James <jam3sstotomas@gmail.com>
2024-01-03 23:12:17 -0500

    fixed issue with missing start scripts in package.json

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Home from './components/Home'
 function App() {
     return (
         <>
-            <head>
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
-            </head>
-
             <Home />
         </>
     );
