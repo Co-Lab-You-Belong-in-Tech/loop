6e419fd8a2b6781b54620b1f5cb388af3813451c
James <jam3sstotomas@gmail.com>
2024-01-21 10:48:34 -0500

    accepted incoming changes from james-dev

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import './App.css';
 import Home from './components/Home'
 import { Link, Route, Routes } from 'react-router-dom';
@@ -53,7 +29,6 @@ function App() {
         </LocalizationProvider>
         </>
     );
->>>>>>> james-dev
 }
 
 export default App;
